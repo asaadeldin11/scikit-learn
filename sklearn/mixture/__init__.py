463b549f43ac1d83cef43a05813ee4766b0f6ea1
Ali Saad-Eldin <ali.saadeldin11@gmail.com>
2020-08-07 10:02:56 -0400

    Update __init__.py

--- sklearn/mixture/__init__.py
+++ sklearn/mixture/__init__.py
@@ -2,9 +2,10 @@
 The :mod:`sklearn.mixture` module implements mixture modeling algorithms.
 """
 
-from ._gaussian_mixture import GaussianMixture
+from ._gaussian_mixture import GaussianMixture, GaussianMixtureCV
 from ._bayesian_mixture import BayesianGaussianMixture
 
 
 __all__ = ['GaussianMixture',
+			'GaussianMixtureCV',
            'BayesianGaussianMixture']
